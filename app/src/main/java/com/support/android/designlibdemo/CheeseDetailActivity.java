c1e2df3772b652ffa53609062e5dd1508919eac4
ccjohnn <cchaixd@163.com>
2017-12-14 11:06:37 +0800

    更改测试

--- app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
@@ -18,6 +18,7 @@ package com.support.android.designlibdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -26,17 +27,20 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-
-import java.util.Random;
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+import com.support.android.designlibdemo.util.Utils;
 
 public class CheeseDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_NAME = "cheese_name";
+    private TwinklingRefreshLayout refreshLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        Utils.onActivityCreateSetTheme(this);//重建Activity，添加主题，但只能更改这个Activity的theme
+        setContentView(R.layout.activity_detail);//如果要更改其他的就要在相应位置调用这个函数，还要重新开始Activity
 
         Intent intent = getIntent();
         final String cheeseName = intent.getStringExtra(EXTRA_NAME);
@@ -45,21 +49,76 @@ public class CheeseDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(cheeseName);
 
-        loadBackdrop();
+        loadBackdrop();//设置toolbar后面的imageView
+
+        //这是更改
+        //下拉不兼容，有刷新但布局有问题
+       /* refreshLayout = (TwinklingRefreshLayout) findViewById(R.id.refreshLayout);
+        refreshLayout.setOnRefreshListener(new RefreshListenerAdapter() {
+            @Override
+            public void onRefresh(final TwinklingRefreshLayout refreshLayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.finishRefreshing();
+                    }
+                }, 2000);
+            }
+
+            @Override
+            public void onLoadMore(final TwinklingRefreshLayout refreshLayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.finishLoadmore();
+                    }
+                }, 2000);
+            }
+        });*/
     }
 
     private void loadBackdrop() {
         final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        //centerCrop按图片比例缩放
         Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(imageView);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        getMenuInflater().inflate(R.menu.theme_menu, menu);
         return true;
+    }
+
+    //new add
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home://这是按左上角的导航键触发返回
+                finish();
+                overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);//还有退出和进入动画，动画只能在startActivity和finish后加入
+                //设置切换动画，从右边进入，左边退出
+            case R.id.menu_demo:
+                Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+                break;
+            case R.id.menu_green:
+                Utils.changeToTheme(this, Utils.THEME_GREEN);
+                break;
+            case R.id.menu_system:
+                Utils.changeToTheme(this, Utils.THEME_DESIGN);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //下面是点击返回按键是触发 new add
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
     }
 }
